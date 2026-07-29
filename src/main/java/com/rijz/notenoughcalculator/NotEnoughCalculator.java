fb5755125dbd33a303f05974bf94d642686e11ba
LazeXD <ryzzers69@gmail.com>
2026-07-29 13:24:40 +0530

    Clean up comments

--- src/main/java/com/rijz/notenoughcalculator/NotEnoughCalculator.java
+++ src/main/java/com/rijz/notenoughcalculator/NotEnoughCalculator.java
@@ -22,9 +22,6 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// Main mod entry point
-// Most of the actual work happens on the client side
-// Created by Rijz & Laze
 public class NotEnoughCalculator implements ModInitializer {
 
     public static final String MOD_ID = "notenoughcalculator";
@@ -32,8 +29,7 @@ public class NotEnoughCalculator implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        LOGGER.info("Not Enough Calculator loading...");
-        // All the real initialization happens in NotEnoughCalculatorClient
-        // since this is a client-side only mod
+        LOGGER.info("Not Enough Calculator initialized");
+        // Client-side initialization happens in NotEnoughCalculatorClient
     }
 }

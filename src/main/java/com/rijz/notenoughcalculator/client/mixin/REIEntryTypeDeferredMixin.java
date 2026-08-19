62ed803da1db1a3d7f7d9d540e4aaaa4d31f8e74
Rijzzz <amaragencyagra@gmail.com>
2026-08-19 11:23:17 +0530

    Add REIEntryTypeDeferredMixin to handle deferred initialization of REI entry types (temp fix)

---
+++ src/main/java/com/rijz/notenoughcalculator/client/mixin/REIEntryTypeDeferredMixin.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of Not Enough Calculator.
+ *
+ * Not Enough Calculator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Not Enough Calculator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.rijz.notenoughcalculator.client.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+@Pseudo
+@Mixin(targets = "me.shedaniel.rei.impl.common.entry.type.EntryTypeDeferred", remap = false)
+public class REIEntryTypeDeferredMixin {
+
+    private static final String REGISTRY_CLASS = "me.shedaniel.rei.api.common.entry.type.EntryTypeRegistry";
+
+    @Inject(method = "getDefinition", at = @At("HEAD"), cancellable = true, remap = false, require = 0)
+    private void onGetDefinition(CallbackInfoReturnable<Object> cir) {
+        try {
+            Object self = this;
+            Field defField = self.getClass().getDeclaredField("definition");
+            defField.setAccessible(true);
+            Object def = defField.get(self);
+
+            if (def == null) {
+                Field idField = self.getClass().getDeclaredField("id");
+                idField.setAccessible(true);
+                Object id = idField.get(self);
+
+                Class<?> registryClass = Class.forName(REGISTRY_CLASS);
+                Method getInstanceMethod = registryClass.getMethod("getInstance");
+                Object registryInstance = getInstanceMethod.invoke(null);
+                Method getMethod = registryClass.getMethod("get", id.getClass());
+                Object typeDef = getMethod.invoke(registryInstance, id);
+
+                if (typeDef == null) {
+                    cir.setReturnValue(null);
+                }
+            }
+        } catch (Throwable ignored) {}
+    }
+}

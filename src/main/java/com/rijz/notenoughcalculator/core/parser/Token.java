3f737ba3fab3d762d1640c53d7963e631f50899d
Rijzzz <amaragencyagra@gmail.com>
2026-08-25 15:50:16 +0530

    Make number field final and add constructor with number value

--- src/main/java/com/rijz/notenoughcalculator/core/parser/Token.java
+++ src/main/java/com/rijz/notenoughcalculator/core/parser/Token.java
@@ -24,11 +24,17 @@ public class Token {
     public final TokenKind kind;
     public final String value;
     public final int pos;
-    public BigDecimal number;
+    public final BigDecimal number;
 
     public Token(TokenKind kind, String value, int pos) {
+        this(kind, value, pos, null);
+    }
+
+    public Token(TokenKind kind, String value, int pos, BigDecimal number) {
         this.kind = kind;
         this.value = value;
         this.pos = pos;
+        this.number = number;
     }
 }
+
